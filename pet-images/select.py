91964fe5260327fa6e8fa1c2678e3f12941e2ed3
suhan8984 <suhan8984@daum.net>
2024-05-06 12:22:53 +0900

    refactor

--- pet-images/select.py
+++ pet-images/select.py
@@ -1,1 +1,10 @@
+import glob
 print("hi")
+def find_idle_gif_files(directory):
+    return glob.glob('./*/idle.gif', recursive=True)
+
+directory = "./"  # 원하는 디렉토리 경로 입력
+idle_gif_files = find_idle_gif_files(directory)
+
+for file_path in idle_gif_files:
+    print(file_path)
